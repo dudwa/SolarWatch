237232c85d4fb7c943a2f61379b0e3c044f0128a
dudwa <dorian.dudas@gmail.com>
2023-11-24 14:38:48 +0100

    feat: Create the ICityRepository class

---
+++ SolarWatch/Service/Repository/ICityRepository.cs
@@ -0,0 +1,13 @@
+using SolarWatch.Data;
+
+namespace SolarWatch.Service.Repository;
+
+public interface ICityRepository
+{
+    IEnumerable<City> GetAll();
+    City? GetByName(string name);
+
+    void Add(City city);
+    void Delete(City city);
+    void Update(City city);
+}
